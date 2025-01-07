a9728a32522b9834af615bc3ee6acf978e6efeb6
Anusha2136 <anushachiraboina5@gmail.com>
2025-01-07 11:44:43 +0530

    Using while

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args) {
+        String username = "anusha001";
+        String pwd = "anusha_001";
+        while((username == "anusha001") && (pwd == "anusha_001")) {
+            System.out.println("Happy new year");
+            break;
+        }
+    }
+}
